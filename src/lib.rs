69c134fb09cbd67d7299c9a206e99f181a6913d1
Boshen <boshenc@gmail.com>
2025-12-22 16:26:56 +0800

    chore: remove doc comment and disable doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,4 @@
 #![allow(clippy::single_match)]
-//! # About
-//!
-//! The main purpose of this library is to format TOML documents while preserving
-//! the original layout, comments, and whitespace where appropriate.
-//!
-//! # Usage
-//!
-//! A TOML document can be formatted directly using the [formatter::format] function:
-//!
-//! ```
-//! use oxc_toml::{format, Options};
-//!
-//! const SOURCE: &str = "value=1\n[table]\nstring='some string'";
-//!
-//! let formatted = format(SOURCE, Options::default());
-//! ```
 
 mod formatter;
 mod lexer;
